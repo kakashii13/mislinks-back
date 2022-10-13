abb2bda999f574950dd1de35d16681d67411e3ae
Matias Dominguez <petunilo96@gmail.com>
2022-10-13 16:36:09 -0300

    add update links

--- controllers/links.js
+++ controllers/links.js
@@ -20,14 +20,12 @@ linkRouter.post("/", async (req, res) => {
   const { title, url, description, category } = req.body;
   const token = tokenExtractor(req);
   const decodedToken = jwt.decode(token);
-  console.log(decodedToken);
   if (!token || !decodedToken) {
     return res.status(401).json({
       error: "token invalid or expired",
     });
   }
   const user = await User.findById(decodedToken.id);
-  console.log(user);
   const link = new Link({
     title,
     url,
@@ -36,9 +34,7 @@ linkRouter.post("/", async (req, res) => {
     fav: false,
     user: user._id,
   });
-
   const linkSaved = await link.save();
-  console.log(linkSaved._id.toJSON(), "link id");
   user.links = user.links.concat(linkSaved._id);
   await user.save();
   res.status(200).json(linkSaved);
@@ -49,9 +45,7 @@ linkRouter.put("/:id", async (req, res) => {
   const { title, url, description, category, fav, user } = req.body;
   const token = tokenExtractor(req);
   const decodedToken = jwt.decode(token);
-  console.log(decodedToken.id, "user id");
-  console.log(user, "user");
-  if (!token || !decodedToken || decodedToken.id !== user) {
+  if (!token || !decodedToken || decodedToken.id !== user.id) {
     return res.status(401).json({
       error: "token invalid or expired",
     });
@@ -73,7 +67,7 @@ linkRouter.put("/:id", async (req, res) => {
 linkRouter.delete("/:id", async (req, res) => {
   //add token for delete
   const id = req.params.id;
-  await Link.findByIdAndRemoves(id);
+  await Link.findByIdAndRemove(id);
   res.status(204);
 });
 
